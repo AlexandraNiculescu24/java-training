f404e8fcfffef2441273b10c624375bae311960a
Alexandra Niculescu <alexandraniculescu@yahoo.com>
2020-05-23 17:11:07 +0300

    cod curs 3

--- _2_oo/src/main/java/code/_3_in_class/Main.java
+++ _2_oo/src/main/java/code/_3_in_class/Main.java
@@ -1,8 +1,17 @@
-package code._3_in_class;
+package Lab2;
 
 public class Main {
-
   public static void main(String[] args) {
-    //TODO put your code changes in here
+    Lab2.Example example=new Lab2.Example();
+    int b=5;
+    int result=example.calculate(b);
+    System.out.println("result= " + result);
+    System.out.println("b= " + b);
+
+    Lab2.Example e = new Lab2.Example();
+    System.out.println("before e.a"+e.getA());
+    example.incrementA(e);
+    System.out.println("after e.a="+e.getA());
+
   }
 }
